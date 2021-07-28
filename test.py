94a2c8b861a8786e3ffec4eebebc31778c8bedba
madserrano <madserrano12@gmail.com>
2021-07-28 17:38:18 -0400

    updating travis yml

--- test.py
+++ test.py
@@ -2,6 +2,15 @@ a = 5
 b = 10
 c = a + b
 
-
-
+if c >=10:
+    c = c + 1
+    # Swap the values
+    a = a + b
+    b = a - b
+    a = a - b
+else:
+    c = c + 10
+    temp = a
+    a = b
+    b = temp
 
