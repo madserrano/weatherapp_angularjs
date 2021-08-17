0182b8d333225249c76521887b52c90f5df53a94
madserrano <madserrano12@gmail.com>
2021-08-17 13:40:49 -0400

    fixing codecov

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,6 @@ module.exports = function(config) {
 	require('karma-jasmine'),
     require('karma-chrome-launcher'),
     require('karma-remap-istanbul'),        
-    require('@angular/cli/plugins/karma')
 	require('@angular-devkit/build-angular/plugins/karma')
 	],
 
