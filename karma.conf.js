452c57401afb9efe7ee7a2cbf7e5bcbb8773efbe
madserrano <madserrano12@gmail.com>
2021-08-18 01:13:48 -0400

    fixed code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -9,16 +9,19 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/team-rocket-swt'),
+      dir: require('path').join(__dirname, './coverage'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
+    },
+	mime: {
+      'text/x-typescript': ['ts','tsx']
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
