f7856c4fd430712d57197e7355952238460dc949
iamAjayDahiya <84476227+iamAjayDahiya@users.noreply.github.com>
2021-08-18 15:38:10 -0400

    Update about.component.ts

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -16,6 +16,20 @@ import { Component, OnInit } from '@angular/core';
 	    We are Team Rocket and our mission is to build this simple web app using AngularJS, integrated with Open Weather map API.
 	  </div>
 	</section>
+    <section class="section">
+    <div>
+      <h3 class="title">Group Members:</h3>
+    </div>
+    <div class="content">
+      <ul>
+        <li class="subtitle">Ajay Dahiya <a href="mailto:C0783939@mylambton.ca" class="icon">📧</a></li>
+        <li class="subtitle">Akshita Khatri <a href="mailto:C0785493@mylambton.ca"class="icon">📧</a></li>
+        <li class="subtitle">Angelica Serrano <a href="mailto:C0785370@mylambton.ca"class="icon">📧</a></li>
+        <li class="subtitle">Jonatas Aguiar <a href="mailto:C0790419@mylambton.ca"class="icon">📧</a></li>
+        <li class="subtitle">Kuldeep Singh <a href="mailto:C0790301@mylambton.ca"class="icon">📧</a></li>
+      </ul>
+    </div>
+    </section>
   `,
   styles: [
   ]
