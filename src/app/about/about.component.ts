3a6de75476eca6712ba51b9902db0b07ef551986
madserrano <madserrano12@gmail.com>
2021-08-18 13:52:37 -0400

    build now

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
         <div class="container">
           <h1 class="title">About Us!</h1>
         </div>
+		
       </div>
     </section>
     <section class="section">
+	  <div class="container">
+	    We are Team Rocket and our mission is to build this simple web app using AngularJS, in which none of us haven't worked on yet before.
+	  </div>
+	</section>
   `,
   styles: [
   ]
